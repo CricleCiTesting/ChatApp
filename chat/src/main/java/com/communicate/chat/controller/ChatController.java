d5189c799ce90afe812eb38b5ac9a15fe77fe93b
Sandeep Yadav <sdyadav7049@gmail.com>
2025-08-09 01:41:49 +0530

    commiting all files now

---
+++ chat/src/main/java/com/communicate/chat/controller/ChatController.java
@@ -0,0 +1,24 @@
+package com.communicate.chat.controller;
+
+import com.communicate.chat.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message){
+        System.out.println(message.getSender());
+        return message ;
+    }
+
+    @GetMapping("/")
+    public String chat(){
+        return "chat" ;
+    }
+
+}
